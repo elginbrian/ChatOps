e8b88d4e36537b36b2cf6a4e789a959fe5a35e83
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-06-07 10:41:34 +0700

    fix(config): env is no longer required

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 class Config:
-    SERVER_HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
-    SERVER_PORT = int(os.environ.get('SERVER_PORT', 5000))
-    COMMAND_TIMEOUT = int(os.environ.get('COMMAND_TIMEOUT', 15))
-    CHATOP_CONTAINER_NAME = os.environ.get('CHATOP_SERVICE_NAME_OR_ID')
-    DEBUG_MODE = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1', 't')
+    SERVER_HOST = '0.0.0.0'
+    SERVER_PORT = 5000
+    COMMAND_TIMEOUT = 15
+    CHATOP_CONTAINER_NAME = 'chatops_service'
+    DEBUG_MODE = True
 
 config = {
     'default': Config
